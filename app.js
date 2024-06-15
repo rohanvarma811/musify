ae25a0e9a1a85fc816e6ab054c8f7009a3e51fae
Rohan <rohanvarma911@gmail.com>
2024-06-16 00:18:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+/**
+ * @license Apache-2.0
+ * @copyright rohanvarma811 2024
+ */
+
+'use strict';
+
+/**
+ * node Modules
+ */
+
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+
+
+/**
+ * Custom Modules
+ */
+const login = require('./src/routes/login.route');
+
+
+// Initial express app
+const express = require('express');
+const app = express();
+
+
+/**
+ * EJS setting
+ */
+app.set('view engine', 'ejs');
+
+
+/**
+ * Setting static Directory
+ */
+app.use(express.static('${__dirname}/public'));
+
+
+/**
+ * Enable cors and cookie Parser
+ */
+app.use(cors()).use(cookieParser());
+
+
+/**
+ * Login Page
+ */
+app.use('/login', login);
+
+
+app.listen(5000, () => {
+    console.log('Server listening at http://localhost:5000');
+});
