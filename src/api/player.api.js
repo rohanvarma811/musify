aa88ebc1da88472bbe15e079021961e1c327d3ea
Rohan <rohanvarma911@gmail.com>
2024-06-29 00:15:20 +0530

    Bottom Navigation and Styling

---
+++ src/api/player.api.js
@@ -0,0 +1,28 @@
+/**
+ * @license Apache-2.0
+ * @copyright rohanvarma811 2024
+ */
+
+'use strict';
+
+/**
+ * custom modules
+ */
+const { getData } = require('../config/axios.config');
+const apiConfig = require('../config/api.config');
+
+
+/**
+ * 
+ * @param {Object} req - server request object 
+ * @param {*} itemLimit - the maximum number of items to return default: 30
+ * @returns {Object}
+ */
+const getRecentlyPlayed = async (req, itemLimit = apiConfig.DEFAULT_LIMIT) => {
+    const { data: recentlyPlayed } = await getData(`/me/player/recently-played?limit=${itemLimit}`, req.cookies.access_token);
+    
+    return recentlyPlayed;
+}
+
+
+module.exports = { getRecentlyPlayed };
