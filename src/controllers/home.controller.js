0fcc430155db9a9eebea5278ea8db7e2b9abd50a
Rohan <rohanvarma911@gmail.com>
2024-07-06 14:23:41 +0530

    Added - #FEATURED PLAYLISTS

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -56,45 +56,45 @@ const trackApi = require('../api/track.api');
 const apiConfig = require('../config/api.config');
 const artisApi = require('../api/artist.api');
 const albumApi = require('../api/album.api');
+const playlistApi = require('../api/playlist.api');
 
 const home = async (req, res) => {
-    try {
-        const currentProfile = await userApi.getProfile(req);
-        // console.log('Current Profile:', currentProfile);
+    const currentProfile = await userApi.getProfile(req);
+    // console.log('Current Profile:', currentProfile);
 
-        const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
-        // console.log('Recently Played:', recentlyPlayed);
+    const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
+    // console.log('Recently Played:', recentlyPlayed);
 
-        const recentlyPlayedTracks = recentlyPlayed.items.map(({ track }) => track);
-        // console.log('Recently Played Tracks:', recentlyPlayedTracks);
+    const recentlyPlayedTracks = recentlyPlayed.items.map(({ track }) => track);
+    // console.log('Recently Played Tracks:', recentlyPlayedTracks);
 
-        const trackIds = recentlyPlayedTracks.map(({ id }) => id);
-        const trackSeed = trackIds.slice(0, 5).join(',');
-        // console.log('Track Seed:', trackSeed);
+    const trackIds = recentlyPlayedTracks.map(({ id }) => id);
+    const trackSeed = trackIds.slice(0, 5).join(',');
+    // console.log('Track Seed:', trackSeed);
 
-        const recommendedAlbums = await trackApi.getRecommendedTrack(req, trackSeed, apiConfig.LOW_LIMIT);
-        // console.log('Recommended Albums:', recommendedAlbums);
+    const recommendedAlbums = await trackApi.getRecommendedTrack(req, trackSeed, apiConfig.LOW_LIMIT);
+    // console.log('Recommended Albums:', recommendedAlbums);
 
 
-        // recommended artists
-        const artistIdEntries = recommendedAlbums.map(track => track.artists.map(artist => artist.id));
-        const uniqueArtistIds = [...new Set(artistIdEntries.flat(1))].join(',');
-        const recommendedArtists = await artisApi.getSeveralDetail(req, uniqueArtistIds);
+    // recommended artists
+    const artistIdEntries = recommendedAlbums.map(track => track.artists.map(artist => artist.id));
+    const uniqueArtistIds = [...new Set(artistIdEntries.flat(1))].join(',');
+    const recommendedArtists = await artisApi.getSeveralDetail(req, uniqueArtistIds);
 
-        // new release albums
-        const newRelease = await albumApi.getNewRelease(req, apiConfig.LOW_LIMIT);
+    // new release albums
+    const newRelease = await albumApi.getNewRelease(req, apiConfig.LOW_LIMIT);
 
-        res.render('./pages/home', {
-            currentProfile,
-            // recentlyPlayedTracks,
-            recommendedAlbums,
-            recommendedArtists,
-            newRelease
-        });
-    } catch (error) {
-        console.error('Error in home controller:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    // featured playlists
+    const featuredPlaylist = await playlistApi.getFeatured(req, apiConfig.LOW_LIMIT);
+
+    res.render('./pages/home', {
+        currentProfile,
+        // recentlyPlayedTracks,
+        recommendedAlbums,
+        recommendedArtists,
+        newRelease,
+        featuredPlaylist
+    }); 
 };
 
 module.exports = { home };
