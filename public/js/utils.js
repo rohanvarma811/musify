30ff1e27dacb8e19a67394bb74ad36b87a3d9cff
Rohan <rohanvarma911@gmail.com>
2024-08-06 20:52:28 +0530

    Home media queries Completed

--- public/js/utils.js
+++ public/js/utils.js
@@ -13,5 +13,15 @@ const addEventOnElems = function(elements, eventType, callback) {
     elements.forEach(element => element.addEventListener(eventType,callback));
 }
 
+const msToTimeCode = (ms) => {
+    const /** {number} */ sec = Math.floor((ms % 60000) / 1000);
+    const /** {number} */ min = Math.floor((ms % 3600000) / 60000);
 
-export { addEventOnElems }
+    const /** {string} */ formattedSec = sec.toString().padStart(2, '0');
+    const /** {string} */ formattedMin = min.toString().padStart(2, '0');
+
+    return `${formattedMin}:${formattedSec}`;
+}
+
+
+export { addEventOnElems, msToTimeCode }
