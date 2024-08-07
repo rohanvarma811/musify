68a78acd6c9e975355b04a42cd39dfa747a07425
Rohan <rohanvarma911@gmail.com>
2024-08-07 20:41:28 +0530

    Completed Styling

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -9,46 +9,6 @@
 /**
  * custom modules
  */
-// const apiConfig = require('../config/api.config');
-// const userApi = require('../api/user.api');
-// const playerApi = require('../api/player.api');
-// const trackApi = require('../api/track.api');
-
-// --- ORIGINAL CODE ---
-// const home = async (req, res) => {
-
-//     // current user Profile
-//     const currentProfile = await userApi.getProfile(req);
-
-//     // recently played tracks
-//     const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
-//     const recentlyPlayedTracks = recentlyPlayed.items.map(({ track }) => track);
-
-//     // recommend albums
-//     const trackIds = recentlyPlayedTracks.map(({ id }) => id);
-//     const trackSeed = trackIds.slice(0, 5).join(',');
-//     const recommendedAlbums = await trackApi.getRecommendedTrack(req, trackSeed, apiConfig.LOW_LIMIT)
-
-//     console.log(recommendedAlbums);
-
-//     res.render('./pages/home', {
-//         currentProfile,
-//         recentlyPlayedTracks,
-//         recommendedAlbums
-//     });
-// }
-
-
-// module.exports = { home };
-
-
-
-// --- REFERENCE CODE ---
-// 'use strict';
-
-/**
- * custom modules
- */
 
 const userApi = require('../api/user.api');
 const playerApi = require('../api/player.api');
